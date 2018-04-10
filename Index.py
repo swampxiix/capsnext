34f08cac8b997685ab797ee0203cc9309b25cce9
Gary Perez <gary.perez@asti-usa.com>
2018-04-10 09:36:11 -0400

    Update schedules for the playoffs, fix traceback.

--- Index.py
+++ Index.py
@@ -14,19 +14,22 @@ class Index (Page):
 
     def writeContent(self):
         wr = self.writeln
-        where, day_of_week, game_date, opponent, game_time = get_next_game(my_sked_file)
-        wr('<div id="content">')
-        h1class = 'future'
-        if day_of_week == 'TODAY':
-            h1class = 'today'
+        try:
+            where, day_of_week, game_date, opponent, game_time = get_next_game(my_sked_file)
+            wr('<div id="content">')
+            h1class = 'future'
+            if day_of_week == 'TODAY':
+                h1class = 'today'
 
-        wr('<h1 class="%s"><img src="capslogo.png"> NEXT GAME</h1>' % (h1class))
-        wr('<h3>%s<br>' % (day_of_week))
-        wr('%s<br>%s PM</h3>' % (game_date, game_time))
-        wr('<h2>%s<br>' % (where.upper()))
-        imgname = opponent.replace('.', '')
-        imgname = imgname.replace(' ', '')
-        imgname = imgname.lower()
-        wr('<img src="logos_hockey/%s.gif"><br>' % (imgname))
-        wr('%s</h2>' % (opponent))
-        wr('</div>')
+            wr('<h1 class="%s"><img src="capslogo.png"> NEXT GAME</h1>' % (h1class))
+            wr('<h3>%s<br>' % (day_of_week))
+            wr('%s<br>%s PM</h3>' % (game_date, game_time))
+            wr('<h2>%s<br>' % (where.upper()))
+            imgname = opponent.replace('.', '')
+            imgname = imgname.replace(' ', '')
+            imgname = imgname.lower()
+            wr('<img src="logos_hockey/%s.gif"><br>' % (imgname))
+            wr('%s</h2>' % (opponent))
+            wr('</div>')
+        except TypeError:
+            wr('No upcoming games.')
